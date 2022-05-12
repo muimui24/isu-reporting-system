03befcef320a3587e3d6c26357d73bf5175dc1d2
juankibin <juankibin@gmail.com>
2022-05-13 07:28:39 +0800

    formkit include

--- isu-reporting-system/src/main.js
+++ isu-reporting-system/src/main.js
@@ -3,10 +3,11 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import { plugin, defaultConfig } from '@formkit/vue'
 
 loadFonts()
 
 createApp(App)
   .use(router)
-  .use(vuetify)
+  .use(vuetify, plugin, defaultConfig)
   .mount('#app')
